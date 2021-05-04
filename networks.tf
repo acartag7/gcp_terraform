71eb611568489619b98631e91fabf650bbb5b23d
Arnold Cartagena <awca22@gmail.com>
2021-05-04 23:02:20 +0200

    aadd

--- networks.tf
+++ networks.tf
@@ -17,7 +17,7 @@ module "network" {
 }
 
 module "network_fabric-net-firewall" {
-    source = "terraform-google-modules/network/google/modules/fabric-net-firewall"
+    source = "terraform-google-modules/network/google//modules/fabric-net-firewall"
     project_id = var.project
     network = module.network.network_name
     internal_ranges_enabled = true
