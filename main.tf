66b5dcd607c8c8cd722f8248c077be495ed245e6
Arnold Cartagena <awca22@gmail.com>
2021-05-04 21:27:14 +0200

    added main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "google" {
+    credentials = "terraform-key.json"
+
+    project = "playground-s-11-d8f0bc2e"
+    region = "us-central1"
+    zone = "us-central-c"  
+}
+
+resource "google_compute_network" "vpc_network" {
+    name = "main-deployment-network"
+}
