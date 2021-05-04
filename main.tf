83d7bd83fc4c62aa6e2772b30840248ce4e30806
Arnold Cartagena <awca22@gmail.com>
2021-05-04 21:55:43 +0200

    added instances

--- main.tf
+++ main.tf
@@ -17,3 +17,24 @@ terraform {
         credentials = "terraform-key.json"
     }
 }
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance"
+    machine_type = "f1.micro"
+
+    boot_disk {
+      initialize_params {
+          image = "debian-cloud/debian-9"
+      }
+    }
+    network_interface {
+      network = "google_compute_network.vpc_network.name"
+      access_config {
+        
+      }
+    }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+    name = "tf-static-ip"
+}
