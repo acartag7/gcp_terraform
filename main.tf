b543fc3e4a2a4b20319be8f7595ee32fe4aec977
root <root@7c0c27b5281c.mylabserver.com>
2021-05-04 20:00:51 +0000

    Added Fix

--- main.tf
+++ main.tf
@@ -1,40 +1,40 @@
 provider "google" {
-    credentials = "terraform-key.json"
+  credentials = "terraform-key.json"
 
-    project = "playground-s-11-d8f0bc2e"
-    region = "us-central1"
-    zone = "us-central-c"  
+  project = "playground-s-11-d8f0bc2e"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
-    name = "main-deployment-network"
+  name = "main-deployment-network"
 }
 
 terraform {
-    backend "gcs" {
-        bucket = "terraform_state_0504"
-        prefix = "Terraform-Info"
-        credentials = "terraform-key.json"
-    }
+  backend "gcs" {
+    bucket      = "terraform_state_0504"
+    prefix      = "Terraform-Info"
+    credentials = "terraform-key.json"
+  }
 }
 
 resource "google_compute_instance" "vm_instance" {
-    name = "terraform-instance"
-    machine_type = "f1.micro"
+  name         = "terraform-instance"
+  machine_type = "f1.micro"
 
-    boot_disk {
-      initialize_params {
-          image = "debian-cloud/debian-9"
-      }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
     }
-    network_interface {
-      network = "google_compute_network.vpc_network.name"
-      access_config {
-        
-      }
+  }
+  network_interface {
+    network = "google_compute_network.vpc_network.name"
+    access_config {
+
     }
+  }
 }
 
 resource "google_compute_address" "vm_static_ip" {
-    name = "tf-static-ip"
+  name = "tf-static-ip"
 }
